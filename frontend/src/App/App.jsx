3f76f98b254076dbeaebb42e26ee6897edaadabe
Ihor849 <Marchelo849@outlook.com>
2023-09-13 14:55:04 +0300

    3

--- frontend/src/App/App.jsx
+++ frontend/src/App/App.jsx
@@ -5,7 +5,7 @@ import { Container } from '../components/Container/Container';
 import PrivateRoute from '../routes/PrivateRoute';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
-import  authOperations  from '../redux/auth/auth-operations';
+import authOperations from '../redux/auth/auth-operations';
 import PublicRoute from '../routes/PublicRoute';
 import { useAll } from '../hooks/useAll';
 import { getRefreshing } from '../redux/auth/auth-selectors';
@@ -24,103 +24,100 @@ const RegisterPage = lazy(() => import('../pages/RegisterPage'));
 const LoginPage = lazy(() => import('../pages/LoginPage'));
 
 const App = () => {
-    const dispatch = useDispatch();
-    const { theme, language } = useAll();
-    const regreshinggAuth = useSelector(getRefreshing);
-    const loadingPets = useSelector(getPetsLoading);
-    document.documentElement.setAttribute('data-theme', theme);
-    document.documentElement.setAttribute('data-lang', language);
+  const dispatch = useDispatch();
+  const { theme, language } = useAll();
+  const regreshinggAuth = useSelector(getRefreshing);
+  const loadingPets = useSelector(getPetsLoading);
+  document.documentElement.setAttribute('data-theme', theme);
+  document.documentElement.setAttribute('data-lang', language);
 
-    useEffect(() => {
-        dispatch(authOperations.fetchCurrentUser());
-    }, [dispatch]);
+  useEffect(() => {
+    dispatch(authOperations.fetchCurrentUser());
+  }, [dispatch]);
 
-    return (
-        <Container>
-            {regreshinggAuth || loadingPets ? (
-                <DotLoader
-                    style={{
-                        position: 'fixed',
-                        top: '50%',
-                        left: '50%',
-                        zIndex: '999',
-                    }}
-                    color="var(--blue)"
-                    cssOverride={{}}
-                    loading
-                    size={70}
-                />
-            ) : (
-                <>
-                    <ToastContainer
-                        position="top-right"
-                        autoClose={2000}
-                        hideProgressBar={false}
-                        newestOnTop={false}
-                        closeOnClick
-                        rtl={false}
-                        limit={3}
-                        pauseOnFocusLoss
-                        draggable
-                        pauseOnHover
-                        theme={theme}
-                    />
-                    <Routes>
-                        <Route path="/" element={<SharedLayout />}>
-                            <Route index element={<Home />} />
+  return (
+    <Container>
+      {regreshinggAuth || loadingPets ? (
+        <DotLoader
+          style={{
+            position: 'fixed',
+            top: '50%',
+            left: '50%',
+            zIndex: '999',
+          }}
+          color="var(--blue)"
+          cssOverride={{}}
+          loading
+          size={70}
+        />
+      ) : (
+        <>
+          <ToastContainer
+            position="top-right"
+            autoClose={2000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            limit={3}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme={theme}
+          />
+          <Routes>
+            <Route path="/" element={<SharedLayout />}>
+              <Route index element={<Home />} />
 
-                            <Route
-                                path="/register"
-                                element={
-                                    <PublicRoute
-                                        redirectTo="/profile"
-                                        component={<RegisterPage />}
-                                    />
-                                }
-                            />
+              <Route
+                path="/register"
+                element={
+                  <PublicRoute
+                    redirectTo="/profile"
+                    component={<RegisterPage />}
+                  />
+                }
+              />
 
-                            <Route
-                                path="/login"
-                                element={
-                                    <PublicRoute
-                                        redirectTo="/profile"
-                                        component={<LoginPage />}
-                                    />
-                                }
-                            />
+              <Route
+                path="/login"
+                element={
+                  <PublicRoute
+                    redirectTo="/profile"
+                    component={<LoginPage />}
+                  />
+                }
+              />
 
-                            <Route
-                                path="/add-pet"
-                                element={
-                                    <PrivateRoute
-                                        redirectTo="/login"
-                                        component={<AddPetPage />}
-                                    />
-                                }
-                            />
+              <Route
+                path="/add-pet"
+                element={
+                  <PrivateRoute
+                    redirectTo="/login"
+                    component={<AddPetPage />}
+                  />
+                }
+              />
 
-                            <Route
-                                path="/profile"
-                                element={
-                                    <PrivateRoute
-                                        redirectTo="/login"
-                                        component={<UserPage />}
-                                    />
-                                }
-                            />
+              <Route
+                path="/profile"
+                element={
+                  <PrivateRoute redirectTo="/login" component={<UserPage />} />
+                }
+              />
 
-                            <Route path="/notices" element={<NoticesPage />} />
+              <Route path="/notices" element={<NoticesPage />} />
 
-                            <Route path="/news" element={<NewsPage />} />
+              <Route path="/news" element={<NewsPage />} />
 
-                            <Route path="/friends" element={<SponsorsPage />} />
+              <Route path="/friends" element={<SponsorsPage />} />
 
-                            <Route path="*" element={<NotfoundPage />} />
-                        </Route>
-                    </Routes>
-                </>
-            )}
-        </Container>
-    );
+              <Route path="*" element={<NotfoundPage />} />
+            </Route>
+          </Routes>
+        </>
+      )}
+    </Container>
+  );
 };
 export default App;
